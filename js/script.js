f684356c953f6daa16fcafed946109ce73c81c16
Igor <musichound.ge@gmail.com>
2023-01-28 19:13:15 +0400

    djfvnskjvnsdkfjnvsdkfjnvskdfjnvksdjfnvksjdfnvksjdfnvsdfvsd fvs dfv sd fv sdf v sd fv sd fvs dfv sdfvsd fv s Please enter the commit message for your changes. Lines starting :wq :wq

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+// при нажатии на кнопку открывается окно отправки данных
 $('.button').on('click', function() {
   $('.popup').addClass('popup_visible')
 });
+
+// окно отправки данных закрывается при нажатии на крестик
 $('.popup_close').on('click', function() {
   $('.popup').removeClass('popup_visible')
 });
+
+// окно отправки данных закрывается при нажатии в области за ее пределами
 $('.popup')
   .click(function(){
     $('.popup').removeClass('popup_visible')
@@ -12,3 +17,88 @@ $('.popup')
   .click(function(e){
     e.stopPropagation();
   });
+
+// добавление класса для появлении стрелки вверх
+if($(window).scrollTop() > 50) {
+    $('.arrow').addClass('arrow_visible');
+}
+
+// стрелка (появляется/исчезает) при скролле вниз-вверх
+$(window).scroll(function(){
+  if($(this) .scrollTop() > 50){
+      $('.arrow').addClass('arrow_visible');
+  } else {
+      $('.arrow').removeClass('arrow_visible');
+    }
+});
+
+// скролл наверх понажатии на стрелку
+$('.arrow').on('click', function() {
+  $("html, body").animate({scrollTop:0},600);
+  return false;
+});
+
+// ссылка на 4 ключевых направления, ведет на слайдер
+$('.text_link').on('click',function (){
+  var destination = $('#slider_start').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Каталог, ведет на слайдер
+$('#catalogue').on('click',function (){
+  var destination = $('#slider_start').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Замена стекла, ведет на слайдер
+$('#windsheild_replacement').on('click',function (){
+  var destination = $('#slider_start').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Ремонт стекла, ведет на слайдер
+$('#windsheild_repair').on('click',function (){
+  var destination = $('#slider_start').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Гарантии, ведет на слайдер
+$('#guaranty').on('click',function (){
+  var destination = $('#slider_start').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Страховым компания, ведет на форму
+$('#insurance_companies').on('click',function (){
+  var destination = $('#feedback_form').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// ссылка Контакты, ведет на контакты и карту
+$('#contacts_id').on('click',function (){
+  var destination = $('#contacts_block').offset().top; 
+  $('html, body').animate({ scrollTop: destination }, 1000);
+  return false;
+});
+
+// открытие политики по клику на ссылку 
+$('.policy_click').on('click', function(){
+  $('.conf-policy').addClass('conf-policy-visible');
+});
+
+// закрытие политики по клику на крестик
+$('.conf-close').on('click', function(){
+$('.conf-policy').removeClass('conf-policy-visible');
+});
+
+// закрытие политики по клику в областе вне окна
+$(document).click(function(e){
+if ($(e.target).is('.conf-policy'))
+$('.conf-policy').removeClass('conf-policy-visible')
+})
